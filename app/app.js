f133456dc24ba681cbac17a35fc9e19fd3de253a
JoeyGarber <joseph.z.garber@gmail.com>
2022-04-14 15:19:03 -0400

    Minor CSS and HTML changes
    Removed index button, centered things.

--- app/app.js
+++ app/app.js
@@ -19,7 +19,6 @@ $(() => {
 
   // Restaurant listeners
   $('#create-restaurant-form').on('submit', restaurantEvents.onCreateRestaurant)
-  $('#index-restaurants-button').on('click', restaurantEvents.onIndexRestaurants)
   $('#wings-table').on('click', '.show-restaurant-form', helpers.showRestaurantForm)
   $('#wings-table').on('submit', '#restaurant-form', restaurantEvents.onUpdateRestaurant)
   $('#wings-table').on('click', '#delete-restaurant-button', restaurantEvents.onDeleteRestaurant)
