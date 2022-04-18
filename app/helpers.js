6c9cbfbafbbd5c999bf718d0def63e754aae138f
JoeyGarber <joseph.z.garber@gmail.com>
2022-04-18 09:40:48 -0400

    Cited a source

--- app/helpers.js
+++ app/helpers.js
@@ -71,6 +71,8 @@ const hideChangePasswordForm = function () {
   $('#change-password-div').html('')
 }
 
+// Searchbar heavily inspired by this https://www.w3schools.com/howto/howto_js_filter_table.asp
+
 const searchbar = function () {
   const input = document.getElementById('searchbar')
   const filter = input.value.toUpperCase()
